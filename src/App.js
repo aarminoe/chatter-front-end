280db4fe16ee3334914409a373f56d3fd758c9ba
aarminoe <aarminoe@gmail.com>
2022-12-07 20:37:01 -0600

    added messages state and use effect to grab all messages

--- src/App.js
+++ src/App.js
@@ -9,11 +9,18 @@ function App() {
   const [addUser, setAddUser] = useState(false);
   const [allUsers, setAllUsers] = useState(null)
   const [seeUsers, setSeeUsers] = useState(false)
+  const [messages, setMessages] = useState(null)
 
   useEffect(() => {
     fetch("http://localhost:8080/user/all")
     .then(resp => resp.json())
     .then(data => setAllUsers(data))
+  }, [])
+
+  useEffect(() => {
+    fetch("http://localhost:8080/message/all")
+    .then(resp => resp.json())
+    .then(data => setMessages(data))
   }, [])
 
   return (
